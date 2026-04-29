01037bcf14baa74bd5e4476d1e950723c16c769e
Badal <badal@logikview.com>
2026-04-29 14:32:57 +0530

    commit

--- NewWebAPICore/Program.cs
+++ NewWebAPICore/Program.cs
@@ -116,11 +116,20 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "NewWebAPICore v1");
+    c.RoutePrefix = "swagger";
+
+    c.DocumentTitle = "Product Web API - API Documentation";
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
